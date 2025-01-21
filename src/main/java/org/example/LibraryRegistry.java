361a5ee6b78048c00dc26665ff13e6fa61c03d3d
vugarunity <159554692+vugarunity@users.noreply.github.com>
2025-01-21 13:14:30 +0400

    Update LibraryRegistry.java

--- src/main/java/org/example/LibraryRegistry.java
+++ src/main/java/org/example/LibraryRegistry.java
@@ -5,24 +5,20 @@ import java.util.List;
 
 public class LibraryRegistry {
     public static void main(String[] args) {
-        // Создание объектов библиотеки
         List<LibraryItem> items = new ArrayList<>();
         items.add(new Book("001", "Author A"));
         items.add(new Journal("002", "Author B"));
         items.add(new Book("003", "Author A"));
         items.add(new Journal("004", "Author C"));
 
-        // Инициализация сервиса поиска
         SearchService searchService = new SearchService(items);
 
-        // Поиск по инвентарному номеру
         System.out.println("Search by Inventory Number '001':");
         searchService.searchByInventoryNumber("001").forEach(System.out::println);
 
         System.out.println("Search by Inventory Number '002':");
         searchService.searchByInventoryNumber("002").forEach(System.out::println);
 
-        // Поиск по автору
         System.out.println("\nSearch by Author 'Author A':");
         searchService.searchByAuthor("Author A").forEach(System.out::println);
     }
